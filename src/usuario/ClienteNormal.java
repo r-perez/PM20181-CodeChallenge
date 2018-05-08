8aa8930ac8bb5a72f96d3fca6eeba1f4ef95bec0
soniaAlejandra <37317790+soniaAlejandra@users.noreply.github.com>
2018-05-07 22:41:09 -0300

    Update ClienteNormal.java

--- src/usuario/ClienteNormal.java
+++ src/usuario/ClienteNormal.java
@@ -4,8 +4,14 @@ import controle.Carrinho;
 
 public class ClienteNormal extends Cliente {
 
+	// Cria um ClienteNormal com os mesmos atributos da classe mãe
+	
 	public ClienteNormal(long id, String nome, String cpf, String endereco, String telefone) {
+		
 		super(id, nome, cpf, endereco, telefone);
+		
+		// Cria um carrinho com os respectivos valores de desconto para cada tipo de produto
+		
 		super.carrinho = new Carrinho(3, 2, 5);
 	}
 }
